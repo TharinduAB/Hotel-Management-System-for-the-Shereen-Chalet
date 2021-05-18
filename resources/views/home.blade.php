3bf6361675c101f732cea4b40c8d9b0039076f05
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-18 19:25:49 +0530

    css changes for UIs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <link rel="stylesheet" href="/css/createex.css"/>
 <link rel="stylesheet" href="/css/home.css"/>
+<div class="bac">
 <header>
+    
 <div class= "header">
 <a href= "#home">
 <img src= "/pictures/SSLogo.jpg" class="logo" height="80px" width="160px" >
@@ -39,5 +41,6 @@
 <td><a class="btn btn-primary4" href="{{ route('budgets.index') }}" target="_blank"> Plan Budget</a> </td>
 <td><a class="btn btn-primary5" href="{{ route('expenditures.index') }}" target="_blank"> Input Expenditures</a> </td>
 <td><a class="btn btn-primary6" href="{{ route('incomes.index') }}" target="_blank"> Input Incomes</a></td></tr> </table>
+</div> <br><br><br>
 </div>
 @endsection
