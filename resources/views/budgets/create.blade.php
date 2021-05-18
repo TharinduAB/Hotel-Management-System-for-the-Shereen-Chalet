d9436f9b1df6c3835c3f817ae86b381d1801df83
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-18 19:34:50 +0530

    css changes

--- resources/views/budgets/create.blade.php
+++ resources/views/budgets/create.blade.php
@@ -35,7 +35,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Add New Budget Record</h2>
+            <h2>Add New Budget Records</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('budgets.index') }}"> Back</a>
