61d180365fd048f5a30cd09f4f0903f5f9e6c312
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-25 21:46:34 +0530

    Routs added

--- resources/views/budgets/create.blade.php
+++ resources/views/budgets/create.blade.php
@@ -10,21 +10,23 @@
 </div>
 </header>
 <div class            = "topnav">
-<a href             = "#RoomBooking">Room Booking</a>
-<a href             = "#Event">Event Management</a>
-<a href             = "#Emp">Employee Management</a>
-<a href             = "#RoomMana">Room Management</a>
-<a href             = "#Maint">Maintenance</a>
-<a href             = "#Dining">Dining</a>
-<a href             = "#Inv">Inventory</a>
-<a href             = "#Fin">Financial</a>
+    <a href             = "/index">Room Booking</a>
+    <a href             = "eventHome">Event Management</a>
+    <a href             = "{{ route('employees.index') }}">Employee Management</a>
+    <a href             = "RM">Room Management</a>
+    <a href             = "{{ route('housekeepers.index') }}">Maintenance</a>
+    <a href             = "{{ route('dinein.create') }}">Dining</a>
+    <a href             = "InvHome">Inventory</a>
+    <a href             = "/users">Financial</a>
 
 </div>
 <hr class             = "line2">
 <br>
-<a href               = "#home" style="font-family:calibri;font-size:18px;"> Home  </a>
+<a href               = "../SCHome" style="font-family:calibri;font-size:18px;"> Home  </a>
 <text> > </text>
-<a href               = "#Inv" style="font-family:calibri;font-size:18px;"> Financial Management </a>
+<a href               = "/users" style="font-family:calibri;font-size:18px;"> Financial Management </a>
+<text> > </text>
+<a href               = "{{ route('budgets.index') }}" style="font-family:calibri;font-size:18px;"> Budget records </a>
 <text> > </text>
 <a href               = "#New" style="font-family:calibri;font-size:18px;"> New Entry </a>
 <a href               = "#useraccount" target="_blank">
