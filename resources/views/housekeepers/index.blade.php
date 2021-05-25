45641d7cccdbe1bf78551f12d7c1d2ad4875a332
HeliSahanya <heli.sahanya22@gmail.com>
2021-05-25 20:59:50 +0530

    Blade file updated

--- resources/views/housekeepers/index.blade.php
+++ resources/views/housekeepers/index.blade.php
@@ -19,7 +19,7 @@
 
       <div class="row">
         <div class = "col-lg-12"><br>
-        <h3 style="margin-left:50px;"><u><b>List of Housekeeper</b></u></h3>
+        <h3 style="margin-left:50px;"><u><b>List of Housekeepers</b></u></h3>
           <div class="float-right">
             <div class="btn-group" role="group" aria-label="Basic example">
               <a class="btn btn-outline-primary" href = "{{ route('housekeepers.create') }}">Add Housekeeper</a>
