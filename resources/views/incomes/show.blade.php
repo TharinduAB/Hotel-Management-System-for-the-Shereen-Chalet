6e0708e0b56cd526ba75a9945b41d70b367c7da6
dilendranishadhi <dilendranishadi@gmail.com>
2021-05-25 22:13:08 +0530

    routes updated

--- resources/views/incomes/show.blade.php
+++ resources/views/incomes/show.blade.php
@@ -10,12 +10,12 @@
     </header>
     <div class            = "topnav">
         <a href             = "/index">Room Booking</a>
-        <a href             = "eventHome">Event Management</a>
+        <a href             = "/eventHome">Event Management</a>
         <a href             = "{{ route('employees.index') }}">Employee Management</a>
-        <a href             = "RM">Room Management</a>
+        <a href             = "/RM">Room Management</a>
         <a href             = "{{ route('housekeepers.index') }}">Maintenance</a>
         <a href             = "{{ route('dinein.create') }}">Dining</a>
-        <a href             = "InvHome">Inventory</a>
+        <a href             = "/InvHome">Inventory</a>
         <a href             = "/users">Financial</a>
     
     </div>
